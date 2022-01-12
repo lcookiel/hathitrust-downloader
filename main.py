afce254b5690a3615034f3ef48e0ecf0d201f1d8
Lazarus <eleazar.levchenko@gmail.com>
2022-01-12 20:23:20 +0200

    Fix indentation

--- main.py
+++ main.py
@@ -36,52 +36,52 @@ def fullview_check(pagehtml): # check if fullview is available
         return False
 
 
-        def npages(pagehtml): # count item's number of pages
-            npages = int(pagehtml.find("span", {"data-slot": "total-seq"}).get_text())
-            return npages
+def npages(pagehtml): # count item's number of pages
+    npages = int(pagehtml.find("span", {"data-slot": "total-seq"}).get_text())
+    return npages
 
 
-        def save_and_merge(item_url):
-            # get html of item page
-            pagehtml = getpagehtml(item_url)
-            # if fullview is available, download and merge all pages
-            if fullview_check(pagehtml):
-                npages = npages(pagehtml)
-                # get item id
-                    id = pagehtml.find("a", {"class": "page-pdf-link"})["href"]
-                    parsed = urlparse.urlparse(id)
-                    id = parse_qs(parsed.query)['id'][0]
-                    print("Item ID: "+id)
-                    # create empty list of pages that will be merged
-                    pages = []
-                    # save every page as pdf
-                    for i in range(1, npages+1):
-                        time.sleep(1)
-                        get_pdf("https://babel.hathitrust.org/cgi/imgsrv/download/pdf?id="+id+";orient=0;size=100;seq="+str(i)+";attachment=0", "page"+str(i))
-                              # filter broken files by size
-                              size = os.stat("page"+str(i)+".pdf").st_size
-                              # if less than 10000 bytes, then supposedly broken
-                              while size < 10000:
-                                  print("Size of file is less than 10000 bytes, so it must be broken, repeating.")
-                                  os.remove("page"+str(i)+".pdf")
-                                  get_pdf("https://babel.hathitrust.org/cgi/imgsrv/download/pdf?id="+id+";orient=0;size=100;seq="+str(i)+";attachment=0", "page"+str(i))
-                                  size = os.stat("page"+str(i)+".pdf").st_size
-                                  time.sleep(1)
-                              # append filename of the page to the list
-                              pages.append("page"+str(i)+".pdf")
-                              print(str(i)+"/"+str(npages)+" pages downloaded.", end="\r", flush=True)
-                                # merge files into one pdf
-                                print("Merging pages into one file.")
-                                merger = PdfFileMerger()
-                                for pdf in pages:
-                                    merger.append(pdf)
-                                merger.write(id+".pdf")
-                                merger.close()
-                                print("File "+id+".pdf "+"is done.")
-                                for page in pages:
-                                    os.remove(page)
-                            else:
-                                  print("Full View is not available.")
+def save_and_merge(item_url):
+    # get html of item page
+    pagehtml = getpagehtml(item_url)
+    # if fullview is available, download and merge all pages
+    if fullview_check(pagehtml):
+        npages = npages(pagehtml)
+        # get item id
+        id = pagehtml.find("a", {"class": "page-pdf-link"})["href"]
+        parsed = urlparse.urlparse(id)
+        id = parse_qs(parsed.query)['id'][0]
+        print("Item ID: "+id)
+        # create empty list of pages that will be merged
+        pages = []
+        # save every page as pdf
+        for i in range(1, npages+1):
+            time.sleep(1)
+            get_pdf("https://babel.hathitrust.org/cgi/imgsrv/download/pdf?id="+id+";orient=0;size=100;seq="+str(i)+";attachment=0", "page"+str(i))
+        # filter broken files by size
+        size = os.stat("page"+str(i)+".pdf").st_size
+        # if less than 10000 bytes, then supposedly broken
+        while size < 10000:
+            print("Size of file is less than 10000 bytes, so it must be broken, repeating.")
+            os.remove("page"+str(i)+".pdf")
+            get_pdf("https://babel.hathitrust.org/cgi/imgsrv/download/pdf?id="+id+";orient=0;size=100;seq="+str(i)+";attachment=0", "page"+str(i))
+            size = os.stat("page"+str(i)+".pdf").st_size
+            time.sleep(1)
+        # append filename of the page to the list
+        pages.append("page"+str(i)+".pdf")
+        print(str(i)+"/"+str(npages)+" pages downloaded.", end="\r", flush=True)
+        # merge files into one pdf
+        print("Merging pages into one file.")
+        merger = PdfFileMerger()
+        for pdf in pages:
+            merger.append(pdf)
+        merger.write(id+".pdf")
+        merger.close()
+        print("File "+id+".pdf "+"is done.")
+        for page in pages:
+            os.remove(page)
+    else:
+          print("Full View is not available.")
 
 
 if __name__ == '__main__':
